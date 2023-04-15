83abd570606d32e6d82e3412392509e934dfbe91
tictactoe <hi>
2023-04-15 13:13:40 +0530

    Gram schmidth

--- New/Vector_class.h
+++ New/Vector_class.h
@@ -18,6 +18,7 @@ class vec{
     int num;//number of vectors
     bool check;
     double**data;
+    double**gram;//For storing gram schmidth vectors
     public:
     vec(int a,int b):dim(a),num(b){}
     vec(){};
@@ -25,9 +26,10 @@ class vec{
     void set_number(int a){num =a;check = false;}
     void make_space(){
         if(num >0 && dim >0 && check == false){
-            data = (double**)malloc(num*sizeof(double*));
+            data = (double**)malloc(num*sizeof(double*));gram = (double**)malloc(num*sizeof(double*));
             check = true;
-            for(int i=0;i<num;i++){ data[i] = (double*)malloc(dim*sizeof(double));}
+            for(int i=0;i<num;i++){ data[i] = (double*)malloc(dim*sizeof(double));
+            gram = (double**)malloc(num*sizeof(double*));}
         }else{cout<<"Sorry you can't assign memory"<<endl;}
     }
     void input(){
